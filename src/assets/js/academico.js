332d31c48ee5850561511f19334b2f43793af48a
Luciano Sthioul <lucianoaaron.sthioul@alumnos.ulagos.cl>
2025-12-07 13:03:58 -0300

    Ultimos ultimos cambios 2

--- src/assets/js/academico.js
+++
@@ -1,9 +0,0 @@
-
-
-    function toggleCard(e) {
-    const card = e.currentTarget;
-    const extra = card.querySelector('.extra-content');
-    if (extra) {
-    extra.classList.toggle('hidden');
-}
-}
